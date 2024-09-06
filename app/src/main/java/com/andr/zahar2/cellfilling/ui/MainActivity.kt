21b4f1813a692b894b5429271f043c7233882f55
andrz <andr.zahar2@gmai.com>
2024-09-07 01:55:01 +0500

    Add MainActivity

---
+++ app/src/main/java/com/andr/zahar2/cellfilling/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.andr.zahar2.cellfilling.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
